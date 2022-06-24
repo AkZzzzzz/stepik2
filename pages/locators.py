a93aeb1b75500adffea01e2010bf622ca456eb22
AkZzzzzz <64326509+AkZzzzzz@users.noreply.github.com>
2022-06-24 22:45:43 +0300

    Update locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -4,4 +4,6 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 class LoginPageLocators():
-    
+    LOGIN_USERNAME = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+ 
