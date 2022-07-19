f70b871db83441ef843a050dc4ed52ae91005c0c
AkZzzzzz <64326509+AkZzzzzz@users.noreply.github.com>
2022-07-19 15:49:27 +0300

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -3,4 +3,8 @@ from pages.product_page import ProductPage
 
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    
+    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.add_product()  # выполняем метод страницы — переходим на страницу логина
+    #login_page = LoginPage(browser, browser.current_url)
+    #login_page.should_be_login_page()
