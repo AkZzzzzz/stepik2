b70447ff39f4fd15bcb2fa135908c771be34e2cc
AkZzzzzz <64326509+AkZzzzzz@users.noreply.github.com>
2022-07-19 15:24:47 +0300

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -1,1 +1,5 @@
+from pages.main_page import MainPage
 
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    
