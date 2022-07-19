dee055da562f5c57cfca5863de771aa46898ca9f
AkZzzzzz <64326509+AkZzzzzz@users.noreply.github.com>
2022-07-19 15:43:08 +0300

    Update product_page.py

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,1 +1,8 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+from selenium.webdriver.common.by import By
 
+class ProductPage(BasePage):
+  def add_product(self):
+    
+    
